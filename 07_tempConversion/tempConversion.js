0b3c41bf1e1b8661a6b05c1d823de8bbde49ff94
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-02 18:45:01 +0300

    solved exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  new_temp = (temperature - 32) * (5/9);
+  new_temp = new_temp.toFixed(1);
+  new_temp = parseFloat(new_temp);
+  return new_temp
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  new_temp = ((temp * 9/5) + 32);
+  new_temp = new_temp.toFixed(1);
+  new_temp = parseFloat(new_temp);
+  return new_temp
 };
 
 // Do not edit below this line
