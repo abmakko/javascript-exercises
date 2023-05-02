3bbf045fe0ccbb8ccf4fd7d67e3765f886dc55b7
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-02 17:12:57 +0300

    solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 >= 0 && num2 >= 0 && (typeof (num1 + num2) === 'number')){
+        difference = (Math.abs(num1 - num2)) + 1
+
+        sumOfSeries = (difference/2) * (num1 + num2);
+        return sumOfSeries;
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
