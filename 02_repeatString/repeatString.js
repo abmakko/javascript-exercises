e054f35ff58fcd5e23698a5c25a5f78893a55fed
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-02 10:47:24 +0300

    solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringContainer = "";
+    if(num >= 0){
+        for (i = 0; i<num; i++){
+            stringContainer += string;
+        }
+        return stringContainer;
+    }
+    else{
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
