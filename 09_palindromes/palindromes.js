4b9b305a2c0efc2322f9108dd8bbf82229260f67
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-11 09:45:02 +0300

    solved palindrome excersice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+       
+    newString = string.split('');        
+    reverseString = newString.reverse();
+    newString = string.split(/[!,\s:.1234567890=]/);
+    reverseString = reverseString.join('').split(/[!,\s:.1234567890=]/);
+    newString = newString.join('').toLowerCase();
+    reverseString = reverseString.join('').toLowerCase();
 
+    return newString === reverseString ? true : false;
 };
 
 // Do not edit below this line
