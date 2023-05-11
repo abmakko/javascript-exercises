6f18e5e90b3d93f2f978f007f070a2f0fb762ee6
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-11 15:35:01 +0300

    solve the object exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const d = new Date();
+    newperson = people.reduce((accumulator, current) => {
 
+        if (!('yearOfDeath' in current)) current.yearOfDeath = d.getFullYear();
+        if((current.yearOfDeath - current.yearOfBirth) > (accumulator.yearOfDeath - accumulator.yearOfBirth)){
+          return current
+        }
+        else
+          return accumulator  
+      }, {
+        name: "",
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+      })
+    
+      return newperson;
 };
 
 // Do not edit below this line
