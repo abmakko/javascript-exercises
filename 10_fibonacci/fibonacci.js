91d51bc992d3a3930503d36f9d690aff2d9643d0
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-11 11:49:26 +0300

    solved fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if(num < 0){
+        
+        return "OOPS";
+        
+    }
+    else{
+        array = new Array(num).fill(1);
+        for(i = 0; i<num; i++){
+            if (i>1){
+              array[i] = array[i-1] + array[i-2]
+            }
+          }
+          return array[num-1]
+    }
 };
 
 // Do not edit below this line
