d17da0dc064ba701da006aac8f60a555319b2705
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-11 07:23:34 +0300

    solved the calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, current) => accumulator + current, 0,)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, current) => accumulator * current, 1,)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  num === 0 ? 1 : num;
+  arr = new Array(num).fill(0)
+
+  new_arr = arr.map((element, index, array) => num-index);
+  return new_arr.reduce((accum, curr) => accum * curr, 1,);
 	
 };
 
